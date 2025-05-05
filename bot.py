a8c4765f783230365e615a245649fdd6f1de9419
Raaz <161096603+Rajeshexploit@users.noreply.github.com>
2025-05-06 04:57:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ TOKEN = os.environ.get('BOT_TOKEN')
 def start(update, context):
     update.message.reply_text("🎬 VegaMoviesBot चालू है!")
 
-updater = Updater(TOKEN, use_context=True)
-updater.dispatcher.add_handler(CommandHandler('start', start))
+# नया सिंटैक्स (use_context हटाएं)
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.start_polling()
 updater.idle()
